4fd9860f3a09bfbc656d0a29b6f2db789477944f
Nahid-web <nahidamin266@gmail.com>
2025-01-05 21:54:21 +0600

    add docker and config task_backend container

--- taskapp_backend/src/index.ts
+++ taskapp_backend/src/index.ts
@@ -1,1 +1,10 @@
-console.log("hi nahid from Ts again");
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Task App");
+});
+
+app.listen(8000, () => {
+  console.log("Server Run on port 8000");
+});
