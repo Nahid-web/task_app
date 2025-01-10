a03537fe00fa99bd08716ec6f83ad17e66605ecd
Nahid-web <nahidamin266@gmail.com>
2025-01-10 11:36:31 +0600

    store token,,create SpService

--- taskapp_frontend/lib/core/utils.dart
+++ taskapp_frontend/lib/core/utils.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void showSnackBar(BuildContext context, String message) {
   ScaffoldMessenger.of(context).showSnackBar(
@@ -6,4 +7,16 @@ void showSnackBar(BuildContext context, String message) {
       content: Text(message),
     ),
   );
+}
+
+class SpService {
+  Future<void> setToken(String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('x-auth-token', token);
+  }
+
+  Future<String?> getToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('x-auth=token');
+  }
 }
